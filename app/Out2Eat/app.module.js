d933e693433296cc727acb8f0c125da357c9e353
Walter Moore <walteragnewmoore@gmail.com>
2015-11-11 10:20:52 -0600

    added ngAnimate

--- app/Out2Eat/app.module.js
+++ app/Out2Eat/app.module.js
@@ -1,5 +1,5 @@
 var Out2Eat;
 (function (Out2Eat) {
     "use strict";
-    angular.module("Out2Eat", ["ngRoute"]);
+    angular.module("Out2Eat", ["ngRoute", "ngAnimate"]);
 })(Out2Eat || (Out2Eat = {}));
--- app/Out2Eat/app.module.ts
+++ app/Out2Eat/app.module.ts
@@ -1,4 +1,4 @@
 module Out2Eat{
 	"use strict";
-	angular.module("Out2Eat", ["ngRoute"]);
+	angular.module("Out2Eat", ["ngRoute", "ngAnimate"]);
 }
